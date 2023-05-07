e4e7ce356a0d944672ab5a387377f3bf41b61d97
Orton Bright <ortonb110@gmail.com>
2023-05-07 20:26:13 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([])
+  const url = 'https://course-api.com/react-tabs-project';
+
+  const fetchData = async (url) => {
+      try {
+        const response = await fetch(url);
+         const data = await response.json();
+         setData(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+  }
+
+  useEffect(()=>{
+    fetchData(url);
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      Hello World!
     </div>
   );
 }
